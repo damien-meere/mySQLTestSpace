c44e6bd61b57f74687befce2b3b3fb9f374c2cd1
Damien Meere <damien.meere@bt.com>
2020-03-12 12:33:04 +0000

    using python to interact with MongoDB in mongo.py file

--- mongo.py
+++ mongo.py
@@ -1,2 +1,38 @@
 import pymongo
 import os
+
+MONGO_URI = os.getenv("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is Connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MondoDB: %s") % e
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+#Inserting a new record
+#new_doc = {'first': 'douglas', 'last': 'adams', 'dob': '11/03/1952', 'gender': 'm', 'hair_colour': 'grey', 'occupation': 'writer', 'nationality': 'english'}
+#coll.insert(new_doc)
+
+#Inserting a multiple new records
+#new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948', 'gender': 'm', 'hair_colour': 'not much', 'occupation': 'writer', 'nationality': 'english'},
+#            {'first': 'george', 'last': 'rr martin', 'dob': '20/09/1952', 'gender': 'm', 'hair_colour': 'white', 'occupation': 'writer', 'nationality': 'american'}]
+#coll.insert_many(new_docs)
+
+#Delete a single record
+#documents = coll.find({'first': 'douglas'})
+#coll.delete_one({'first': 'douglas'})
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
+
+
